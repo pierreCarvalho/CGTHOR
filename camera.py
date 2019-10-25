7eb65f2857bc90481caeace14d32c1fdef353abc
Pierre Carvalho Fenner <40218734+pierreCarvalho@users.noreply.github.com>
2019-10-25 17:55:35 -0300

    Foi criado a projeção da cena e a criação da camera, ainda está com o problema de escala

--- camera.py
+++ camera.py
@@ -2,8 +2,6 @@ import pyglet
 from pyglet.window import key
 from os import path
 import ratcave as rc
-
-
 #sets e configs
 #tela
 window = pyglet.window.Window()
@@ -11,51 +9,66 @@ window = pyglet.window.Window()
 keys = key.KeyStateHandler()
 window.push_handlers(keys)
 
-# Insert filename into WavefrontReader.
+
+# Inserir o caminho do arquivo do obj
 obj_filename2 = path.join('thorFinal.obj')
-#obj_filename = rc.resources.obj_primitives()
+# Ler o arquivo obj e retorna os pontos
 obj_reader = rc.WavefrontReader(obj_filename2)
 
 # Create Mesh
 thor = obj_reader.get_mesh("thor")
+# Seta a posição inicial, escala, ?
 thor.position.xyz = 0,0,-2
 scale = .1
 thor.scale = scale
+thor.uniforms['diffuse'] = [.5, .0, .8]
 
-# Create Scene
+# Cria a cena
 scene = rc.Scene(meshes=[thor])
 scene.bgColor = 0.4, 0.2, 0.4
+scene.light.position = 0, 3, -1
+# Cria a camera
+camera = rc.Camera(projection=rc.PerspectiveProjection(fov_y=90, aspect=1.))
+scene.camera = camera
+#Faz a projeção da cena
+projected_scene = rc.Scene(meshes=[thor], bgColor=(1., 1., 1.))
+projected_scene.light.position = scene.light.position
+projected_scene.camera = rc.Camera(position=(0, 0, 5), rotation=(0, 0, 0))
+projected_scene.camera.projection.z_far = 50
 
+# Atualiza a movimentação da camera e do objeto
 def move_camera(dt):
-  global scale
-  camera_speed = 3
-  if keys[key.W]:
-    print(thor.scale)
-    thor.scale.x += 0.1
-    thor.scale.y += 0.1
-    thor.scale.z += 0.1
-
-    scene = rc.Scene(meshes=[thor])
-    scene.bgColor = 0.4, 0.2, 0.4
-  if keys[key.S]:
-    thor.scale.x -= 0.1
-    thor.scale.y -= 0.1
-    thor.scale.z += 0.1
-    scene = rc.Scene(meshes=[thor])
-    scene.bgColor = 0.4, 0.2, 0.4
-  if keys[key.RIGHT]:
-      scene.camera.position.x += camera_speed * dt
-  if keys[key.UP]:
-      scene.camera.position.y += camera_speed * dt
-  if keys[key.DOWN]:
-      scene.camera.position.y -= camera_speed * dt
+    global scale
+    global projected_scene
+    camera_speed = 1
+    if keys[key.W]:
+        thor.scale.x += 0.1
+        thor.scale.y += 0.1
+        thor.scale.z += 0.1
+    elif keys[key.S]:
+        thor.scale.z -= 0.1
+        thor.scale.y -= 0.1
+        thor.scale.x -= 0.1
+    elif keys[key.J]:
+        thor.rotation.y += 0.5
+    elif keys[key.U]:
+        thor.rotation.x -= 0.5
+    elif keys[key.K]:
+        thor.rotation.z += 0.5
+    elif keys[key.RIGHT]:
+        projected_scene.camera.position.x += camera_speed * dt
+    elif keys[key.LEFT]:
+        projected_scene.camera.position.x -= camera_speed * dt
+    elif keys[key.UP]:
+        projected_scene.camera.position.y += camera_speed * dt
+    elif keys[key.DOWN]:
+        projected_scene.camera.position.y -= camera_speed * dt
+  
 pyglet.clock.schedule(move_camera)
-
 
 @window.event
 def on_draw():
     with rc.default_shader:
-        scene.draw()
-
+      projected_scene.draw()
 
 pyglet.app.run()
---
+++ textfield.py
@@ -0,0 +1,58 @@
+import pyglet
+import ratcave as rc
+import math, time
+from ratcave.resources import cube_shader
+
+window = pyglet.window.Window(resizable=True)
+
+# Assemble the Virtual Scene
+obj_reader = rc.WavefrontReader(rc.resources.obj_primitives)
+sphere = obj_reader.get_mesh("Sphere", position=(0, 0, 2), scale=0.2)
+sphere.uniforms['diffuse'] = 1, 0, 0
+
+cube = obj_reader.get_mesh("Cube", position=(0, 0, 0), scale=0.2)
+cube.uniforms['diffuse'] = 1, 1, 0
+
+# virtual_scene = rc.Scene(meshes=[sphere, cube], bgColor=(0., 0., 1.))
+virtual_scene = rc.Scene(meshes=[cube, sphere], bgColor=(0., 0., 1.))
+virtual_scene.light.position.xyz = 0, 3, -1
+
+
+cube_camera = rc.Camera(projection=rc.PerspectiveProjection(fov_y=90, aspect=1.))
+virtual_scene.camera = cube_camera
+
+# Assemble the Projected Scene
+monkey = obj_reader.get_mesh("Monkey", position=(0, 0, -1), scale=0.8)
+screen = obj_reader.get_mesh("Plane", position=(0, 0, 1), rotation=(1.5, 180, 0))
+
+projected_scene = rc.Scene(meshes=[monkey, screen, sphere, cube], bgColor=(1., .5, 1.))
+projected_scene.light.position = virtual_scene.light.position
+projected_scene.camera = rc.Camera(position=(0, 4, 0), rotation=(-90, 0, 0))
+projected_scene.camera.projection.z_far = 6
+
+# Create Framebuffer and Textures
+cube_texture = rc.texture.TextureCube(width=1024, height=1024)  # this is the actual cube texture
+cube_fbo = rc.FBO(texture=cube_texture)
+screen.textures.append(cube_texture)
+
+
+
+clock = 0.
+def update(dt):
+    global clock
+    clock += dt
+    monkey.position.x = math.sin(1.3 * clock)
+    virtual_scene.camera.position.xyz = monkey.position.xyz
+    screen.uniforms['playerPos'] = virtual_scene.camera.position.xyz
+    pyglet.clock.schedule(update)
+
+
+@window.event
+def on_draw():
+    with cube_shader:
+        with cube_fbo as fbo:
+            virtual_scene.draw360_to_texture(fbo.texture)
+            projected_scene.draw()
+
+
+pyglet.app.run()
